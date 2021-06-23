2348e1620a2bb2a86b9555fb140e9d93ffbcc358
Tywen <tywen.kelly@gmail.com>
2021-06-22 18:59:37 -0700

    resetting

--- sketch.js
+++ sketch.js
@@ -7,15 +7,11 @@ console.log('ml5 version:', ml5.version)
 let video
 let poseNet
 let pose
-// let x = 1
-// let y = 1
 let easing = 0.08
 const sensY = -2.5
 const sensX = 1
 
 function setup() {
-  // createCanvas(1280, 720)
-
   var myCanvas = createCanvas(1280, 720)
   myCanvas.parent('video-container')
 
@@ -56,7 +52,8 @@ function draw() {
   image(video, 0, 0)
 
   if (pose) {
-    stroke(255)
+    stroke(255, 0, 0)
+    fill(255, 0, 0)
 
     //track nose and both ears
     let nose = createVector(pose.nose.x, pose.nose.y)
@@ -75,16 +72,16 @@ function draw() {
 
     //ease jittery-ness
     let targetX = posX
-    let dx = targetX - x
-    x += dx * easing
+    let dx = targetX - rx
+    rx += dx * easing
 
     let targetY = posY
-    let dy = targetY - y
-    y += dy * easing
+    let dy = targetY - ry
+    ry += dy * easing
     //draw ellipse
-    fill(255)
+
     strokeWeight(1)
-    ellipse(x, y, 100, 100)
+    ellipse(rx, ry, 100, 100)
 
     strokeWeight(24)
     for (let i = 0; i < 5; i++) {
