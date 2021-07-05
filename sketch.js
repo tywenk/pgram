7ec1ff4438d9e4e8a17b880d5a484466e9d88b9e
tywenk <tywen.kelly@gmail.com>
2021-07-04 20:59:49 -0600

    testing preload as off

--- sketch.js
+++ sketch.js
@@ -1,8 +1,4 @@
-// Your code will go here
-
-// Open up your console - if everything loaded properly you should see the version number
-// corresponding to the latest version of ml5 printed to the console and in the p5.js canvas.
-console.log('ml5 version:', ml5.version)
+// console.log('ml5 version:', ml5.version)
 
 let video
 let poseNet
