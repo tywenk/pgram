d3fc7d1c92b93bb86ab115b11145f977d98c0d9d
tywenk <tywen.kelly@gmail.com>
2021-07-06 17:46:07 -0600

    Added loading percentage
    Loading percentage and put a-frame menu switcher and percentage calculator into utils

---
+++ utils.js
@@ -0,0 +1,53 @@
+// import { modelLoaded as posenet } from './sketch.js'
+
+//menu chooser toggle
+document.getElementById('gltf-menu').addEventListener('change', function (e) {
+  const newValue = e.target.value
+  console.log(newValue)
+  activeModel = newValue
+})
+
+// check if a-scene is loaded
+document.addEventListener('DOMContentLoaded', function () {
+  let scene = document.querySelector('a-scene')
+  let splash = document.querySelector('#splash')
+  scene.addEventListener('loaded', function (e) {
+    // splash.style.display = 'none'
+    console.log('a-scene has been loaded')
+  })
+})
+
+// check if all entities are loaded
+document
+  .querySelector('a-entity')
+  .addEventListener('model-loaded', function () {
+    console.log('a-entities have been loaded')
+  })
+
+function checkLoaded() {
+  let counter = 0
+  let ents = document.querySelectorAll('a-entity')
+  let splashLoading = document.getElementById('percentage')
+  let totalGltf = ents.length - 1
+  let percentComplete
+
+  for (let i = 0; i <= ents.length + 1; i++) {
+    console.log('total gltf: ' + totalGltf)
+    ents[i].addEventListener('model-loaded', () => {
+      counter++
+
+      console.log('counter: ' + counter)
+      console.log('total gltf: ' + totalGltf)
+
+      percentComplete = (counter / totalGltf) * 100
+      let percentString = String(Math.round(percentComplete))
+      splashLoading.innerHTML = percentString + '%'
+
+      if (counter >= totalGltf) {
+        splash.style.display = 'none'
+      }
+    })
+  }
+}
+
+checkLoaded()
