99f7a0b0889095e58592b1b6d8dd804b7d2f292a
tywenk <tywen.kelly@gmail.com>
2021-07-06 23:27:42 -0600

    small fixes

--- sketch.js
+++ sketch.js
@@ -63,6 +63,7 @@ function gotPoses(poses) {
 
 function modelLoaded() {
   console.log('poseNet loaded')
+  poseModelLoaded = true
 }
 
 function draw() {
--- utils.js
+++ utils.js
@@ -1,5 +1,3 @@
-// import { modelLoaded as posenet } from './sketch.js'
-
 //menu chooser toggle
 document.getElementById('gltf-menu').addEventListener('change', function (e) {
   const newValue = e.target.value
@@ -24,24 +22,24 @@ document
     console.log('a-entities have been loaded')
   })
 
-//check if camera is available
-navigator.getMedia =
-  navigator.getUserMedia || // use the proper vendor prefix
-  navigator.webkitGetUserMedia ||
-  navigator.mozGetUserMedia ||
-  navigator.msGetUserMedia
+// check if camera is available
+// navigator.getMedia =
+//   navigator.getUserMedia ||
+//   navigator.webkitGetUserMedia ||
+//   navigator.mozGetUserMedia ||
+//   navigator.msGetUserMedia
 
-navigator.getMedia(
-  { video: true },
-  function () {
-    // webcam is available
-    console.log('camera is availabe')
-  },
-  function () {
-    // webcam is not available
-    console.log('camera is NOT availabe')
-  }
-)
+// navigator.getMedia(
+//   { video: true },
+//   function () {
+//     // webcam is available
+//     console.log('camera is availabe')
+//   },
+//   function () {
+//     // webcam is not available
+//     console.log('camera is NOT availabe')
+//   }
+// )
 
 //check progress of downloading and loading GLTFs
 function checkLoaded() {
@@ -51,21 +49,30 @@ function checkLoaded() {
   let totalGltf = ents.length - 1
   let percentComplete
 
-  for (let i = 0; i < ents.length + 1; i++) {
-    ents[i].addEventListener('model-loaded', () => {
-      counter++
+  if (ents[3]) {
+    for (let i = 0; i <= ents.length; i++) {
+      ents[i].addEventListener(
+        'model-loaded',
+        () => {
+          counter++
 
-      //console.log('counter: ' + counter)
-      //console.log('total gltf: ' + totalGltf)
+          //console.log('counter: ' + counter)
+          //console.log('total gltf: ' + totalGltf)
 
-      percentComplete = (counter / totalGltf) * 100
-      let percentString = String(Math.round(percentComplete))
-      splashLoading.innerHTML = percentString + '%'
+          percentComplete = (counter / totalGltf) * 100
+          let percentString = String(Math.round(percentComplete))
+          splashLoading.innerHTML = percentString + '%'
 
-      if (counter >= totalGltf) {
-        splash.style.display = 'none'
-      }
-    })
+          if (counter >= totalGltf) {
+            document.getElementById('splash').style.display = 'none'
+            // document.getElementById('percentage').style.display = 'none'
+            // document.getElementById('instructions').innerHTML =
+            //   'Loading camera...'
+          }
+        },
+        false
+      )
+    }
   }
 }
 
